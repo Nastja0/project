01cfe648dffe72e293ca15bf2fc73cfbd7cf37c5
Nastja0 <anastasizta_mgn3003@inbox.ru>
2022-04-28 20:36:52 +0500

    ---

---
+++ set-cards.js
@@ -0,0 +1,5 @@
+/*
+const browser = document.querySelector('.browser');
+browser.width = window.screen.availWidth;
+browser.height = window.screen.availHeight;
+console.log(browser.width, browser.height)*/
