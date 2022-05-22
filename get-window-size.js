ae595df3ae9dda286dfd88e64e281201fa1c0240
Nastja0 <anastasizta_mgn3003@inbox.ru>
2022-05-22 17:52:42 +0500

    ---

---
+++ get-window-size.js
@@ -0,0 +1,3 @@
+a = document.querySelector(".window");
+document.querySelector(".window").style.height = `${screen.document.documentElement.offsetHeight}px`;
+document.querySelector(".window").style.width = `${screen.document.documentElement.offsetWidth}px`;
--- set-cards.js
+++
@@ -1,5 +0,0 @@
-/*
-const browser = document.querySelector('.browser');
-browser.width = window.screen.availWidth;
-browser.height = window.screen.availHeight;
-console.log(browser.width, browser.height)*/
