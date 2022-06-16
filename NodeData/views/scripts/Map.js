25c1aa6797a67fbd430be805686406db03d8c405
Mixailkys <9041758286@mail.ru>
2022-06-16 17:43:26 +0500

    Module window works

--- NodeData/views/scripts/Map.js
+++ NodeData/views/scripts/Map.js
@@ -3,13 +3,11 @@ console.log(leveltree)
 let allowedNodes = ['1']
 leveltree.forEach(el => {
     el.addEventListener("click", (e)=>{
-        console.log(el.getAttribute('nextid'));
         let node = el;
         let id = node.getAttribute('id');
         if (allowedNodes.indexOf(id) === -1)
             return;
         allowedNodes = node.getAttribute('nextid');
-        console.log(allowedNodes);
         let circle = el.getElementsByTagName('circle').item(0);
         circle.setAttribute('fill','#ff0000');
     });
